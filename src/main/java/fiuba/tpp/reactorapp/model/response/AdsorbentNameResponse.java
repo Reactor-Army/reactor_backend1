efea904862a82829f0efd3fb8a8a16869665021d
matiasReimondo <matiasreimondo@gmail.com>
2021-05-10 21:20:04 -0300

    Busqueda nombres adsorbentes

---
+++ src/main/java/fiuba/tpp/reactorapp/model/response/AdsorbentNameResponse.java
@@ -0,0 +1,42 @@
+package fiuba.tpp.reactorapp.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import fiuba.tpp.reactorapp.entities.Adsorbent;
+import org.apache.commons.lang3.StringUtils;
+
+public class AdsorbentNameResponse {
+
+    private Long id;
+
+    @JsonProperty("nombre")
+    private String name;
+
+    public AdsorbentNameResponse() {
+    }
+
+    public AdsorbentNameResponse(Adsorbent adsorbent){
+        this.id = adsorbent.getId();
+        this.name = formatName(adsorbent);
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String formatName(Adsorbent adsorbent){
+        return StringUtils.capitalize(adsorbent.getName()) + " (" + adsorbent.getParticleSize()+")";
+    }
+}
