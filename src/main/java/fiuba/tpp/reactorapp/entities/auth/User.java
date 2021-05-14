963de4b6df2eee11f71bee6f93924fc461c1d618
matiasReimondo <matiasreimondo@gmail.com>
2021-05-14 11:57:42 -0300

    HotFix user table

--- src/main/java/fiuba/tpp/reactorapp/entities/auth/User.java
+++ src/main/java/fiuba/tpp/reactorapp/entities/auth/User.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(	name = "user",
+@Table(	name = "\"User\"",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "email")
         })
