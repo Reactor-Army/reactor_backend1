5cb2f29c1e2bd3c3759d1da9cb0f8280827059e9
matiasReimondo <matiasreimondo@gmail.com>
2021-05-14 10:03:20 -0300

    Smell

--- src/main/java/fiuba/tpp/reactorapp/entities/Adsorbate.java
+++ src/main/java/fiuba/tpp/reactorapp/entities/Adsorbate.java
@@ -24,7 +24,7 @@ public class Adsorbate {
     private String numberCAS;
     private String formula;
 
-    private static String SINGLE_CHARGE = "1";
+    private static String single_charge = "1";
 
     public Adsorbate() {
     }
@@ -171,8 +171,8 @@ public class Adsorbate {
         if(textFormula.contains(ionChargeText)){
             return textFormula.replace(ionChargeText,"");
         }else{
-            if(ionChargeText.contains(SINGLE_CHARGE)){
-                String singleCharge = ionChargeText.replace(SINGLE_CHARGE,"");
+            if(ionChargeText.contains(single_charge)){
+                String singleCharge = ionChargeText.replace(single_charge,"");
                 return textFormula.replace(singleCharge,"");
             }
             return textFormula;
