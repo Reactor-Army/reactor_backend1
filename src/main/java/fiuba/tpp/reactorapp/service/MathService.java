e6c73baa5fb75bbcdef1570c91593a2945e43a12
matiasReimondo <matiasreimondo@gmail.com>
2021-08-10 15:05:58 -0300

    Regresion con cuadrados minimos como servicio

--- src/main/java/fiuba/tpp/reactorapp/service/MathService.java
+++ src/main/java/fiuba/tpp/reactorapp/service/MathService.java
@@ -1,7 +1,14 @@
 package fiuba.tpp.reactorapp.service;
 
 import fiuba.tpp.reactorapp.entities.Process;
+import fiuba.tpp.reactorapp.model.exception.NotEnoughObservationsException;
+import fiuba.tpp.reactorapp.model.math.Observation;
+import fiuba.tpp.reactorapp.model.math.RegressionResult;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.apache.commons.math3.util.Precision;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MathService {
@@ -28,5 +35,21 @@ public class MathService {
         double c = - flow;
 
         return a * b * c;
+    }
+
+    public RegressionResult calculateRegression(List<Observation> observations) throws NotEnoughObservationsException {
+        if(observations.size() < 2) throw new NotEnoughObservationsException();
+
+        RegressionResult result = new RegressionResult();
+        //Crearla con true, hace que incluya le valor de b en la regresion
+        SimpleRegression regression = new SimpleRegression(true);
+
+        for (Observation observation: observations) {
+            regression.addData(observation.getX(), observation.getY());
+        }
+        result.setIntercept(Precision.round(regression.getIntercept(),2));
+        result.setSlope(Precision.round(regression.getSlope(),2));
+        return result;
+
     }
 }
--- src/test/java/fiuba/tpp/reactorapp/service/MathServiceTests.java
+++ src/test/java/fiuba/tpp/reactorapp/service/MathServiceTests.java
@@ -1,11 +1,17 @@
 package fiuba.tpp.reactorapp.service;
 
 import fiuba.tpp.reactorapp.entities.Process;
+import fiuba.tpp.reactorapp.model.exception.NotEnoughObservationsException;
+import fiuba.tpp.reactorapp.model.math.Observation;
+import fiuba.tpp.reactorapp.model.math.RegressionResult;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -35,6 +41,66 @@ class MathServiceTests {
         Assertions.assertEquals(0.5, volume,0.01);
 
     }
+
+    // Testeamos  y = 2x
+    @Test
+    void testSimpleRegression(){
+        List<Observation> observations = new ArrayList<>();
+        observations.add(new Observation(1,2));
+        observations.add(new Observation(2,4));
+
+        RegressionResult result = mathService.calculateRegression(observations);
+
+        Assertions.assertEquals(2, result.getSlope(),0.01);
+    }
+
+    // Testeamos  y = 2x + 1
+    @Test
+    void testSimpleRegressionWithInterception(){
+        List<Observation> observations = new ArrayList<>();
+        observations.add(new Observation(1,3));
+        observations.add(new Observation(2,5));
+        observations.add(new Observation(0,1));
+
+        RegressionResult result = mathService.calculateRegression(observations);
+
+        Assertions.assertEquals(2, result.getSlope(),0.01);
+        Assertions.assertEquals(1, result.getIntercept(),0.01);
+    }
+
+    // Probamos una regresion con varios datos que sabemos el resultado
+    // y = -0,84 x + 11,4
+    @Test
+    void testRealRegression(){
+        List<Observation> observations = new ArrayList<>();
+        observations.add(new Observation(7,2));
+        observations.add(new Observation(1,9));
+        observations.add(new Observation(10,2));
+        observations.add(new Observation(5,5));
+        observations.add(new Observation(4,7));
+        observations.add(new Observation(3,11));
+        observations.add(new Observation(13,2));
+        observations.add(new Observation(10,5));
+        observations.add(new Observation(2,14));
+
+
+        RegressionResult result = mathService.calculateRegression(observations);
+
+        Assertions.assertEquals(-0.84, result.getSlope(),0.01);
+        Assertions.assertEquals(11.48, result.getIntercept(),0.01);
+    }
+
+    @Test
+    void testNotEnoughtObservations(){
+        List<Observation> observations = new ArrayList<>();
+        observations.add(new Observation(7,2));
+
+        Assertions.assertThrows(NotEnoughObservationsException.class , () -> {
+            mathService.calculateRegression(observations);
+        });
+    }
+
+
 
 
 
