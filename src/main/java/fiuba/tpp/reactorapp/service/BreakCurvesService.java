ac079c7534e736acffbe6a8bfc88f0effcda1f74
matiasReimondo <matiasreimondo@gmail.com>
2021-08-23 22:53:58 -0300

    Refactor parserService

--- src/main/java/fiuba/tpp/reactorapp/service/BreakCurvesService.java
+++ src/main/java/fiuba/tpp/reactorapp/service/BreakCurvesService.java
@@ -10,15 +10,10 @@ import fiuba.tpp.reactorapp.model.request.chemicalmodels.ThomasRequest;
 import fiuba.tpp.reactorapp.model.request.chemicalmodels.YoonNelsonRequest;
 import fiuba.tpp.reactorapp.model.response.chemicalmodels.ThomasResponse;
 import fiuba.tpp.reactorapp.model.response.chemicalmodels.YoonNelsonResponse;
+import fiuba.tpp.reactorapp.service.utils.CSVParserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,42 +25,21 @@ public class BreakCurvesService {
     @Autowired
     private YoonNelsonModelService yoonNelsonModelService;
 
+    @Autowired
+    private CSVParserService csvParserService;
+
     public ThomasResponse calculateByThomas(ThomasRequest request){
-        List<ChemicalObservation> chemicalObservations = parseCSV(request.getObservaciones());
+        List<ChemicalObservation> chemicalObservations = csvParserService.parse(request.getObservaciones());
 
         return thomasModelService.thomasEvaluation(chemicalObservations,request);
     }
 
     public YoonNelsonResponse calculateByYoonNelson(YoonNelsonRequest request){
-        List<ChemicalObservation> chemicalObservations = parseCSV(request.getObservaciones());
+        List<ChemicalObservation> chemicalObservations = csvParserService.parse(request.getObservaciones());
 
         return yoonNelsonModelService.yoonNelsonEvaluation(chemicalObservations,request);
     }
 
-    private List<ChemicalObservation> parseCSV(MultipartFile file){
-        List<ChemicalObservation> chemicalObservations = new ArrayList<>();
-        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
 
-            CsvToBean<ChemicalObservationCSV> csvToBean = new CsvToBeanBuilder<ChemicalObservationCSV>(reader)
-                    .withType(ChemicalObservationCSV.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-
-            List<ChemicalObservationCSV> csvValues = csvToBean.parse();
-
-            for (ChemicalObservationCSV obsCSV: csvValues) {
-                ChemicalObservation obs = new ChemicalObservation();
-                obs.setVolumenEfluente(Double.parseDouble(obsCSV.getVolumenEfluente().replace(",",".")));
-                obs.setRelacionConcentraciones(Double.parseDouble(obsCSV.getRelacionConcentraciones().replace(",",".")));
-                chemicalObservations.add(obs);
-            }
-            return chemicalObservations;
-
-        }catch (IOException e) {
-            throw new ErrorReadingCSVException();
-        }catch(Exception e){
-            throw new InvalidCSVFormatException();
-        }
-    }
 
 }
