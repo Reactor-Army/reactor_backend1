23539986c4c6c41649bdd9b3b0479bc8d6bfea3d
matiasReimondo <matiasreimondo@gmail.com>
2021-10-02 14:52:10 -0300

    Smells

--- src/main/java/fiuba/tpp/reactorapp/service/auth/AuthService.java
+++ src/main/java/fiuba/tpp/reactorapp/service/auth/AuthService.java
@@ -22,13 +22,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class AuthService {
