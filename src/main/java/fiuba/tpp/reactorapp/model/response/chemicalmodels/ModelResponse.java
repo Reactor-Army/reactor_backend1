748bb77c2dcfecb2ae0e7ea05b780418dcbdb5f6
matiasReimondo <matiasreimondo@gmail.com>
2021-10-19 20:39:20 -0300

    idData null

--- src/main/java/fiuba/tpp/reactorapp/model/response/chemicalmodels/ModelResponse.java
+++ src/main/java/fiuba/tpp/reactorapp/model/response/chemicalmodels/ModelResponse.java
@@ -1,11 +1,13 @@
 package fiuba.tpp.reactorapp.model.response.chemicalmodels;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import fiuba.tpp.reactorapp.model.math.Observation;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ModelResponse {
 
     @JsonProperty("R2")
